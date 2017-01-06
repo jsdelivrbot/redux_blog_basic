b8cd29217fb519d590bffc1425949d66667325af
Andy Eccleston <eandy5000@yahoo.com>
2017-01-06 14:50:02 -0600

    writing get posts action and reducer

---
+++ src/components/posts_index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostIndex = (props) => {
+    return (
+        <div>List of posts</div>
+    )
+}
+
+export default PostIndex
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import { Route, indexRoute } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 
 import App from './components/app'
-
-const Greet = () => {
-    return (<div>Hello world</div>)
-}
+import PostIndex from './components/posts_index'
 
 export default (
 <Route path="/" component={App} >
-    <Route path="greet" component={Greet} />
-    <Route path="greet2" component={Greet} />
-    <Route path="greet3" component={Greet} />
+    <IndexRoute component={PostIndex} />
 </Route>
 
 )
