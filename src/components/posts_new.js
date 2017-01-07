894b2e7f5f577526e33f51cd6bfdf119ddc5e3c0
Andy Eccleston <eandy5000@yahoo.com>
2017-01-07 08:57:18 -0600

    redux form set up and post is working

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import { reduxForm } from 'redux-form'
+import { createPost } from '../actions/index'
+
 
 class PostsNew extends Component {
     render() {
         return (
-            <div>
-            <Link to="/" >Home</Link><br/>
-            Create Form
-            </div>
+            <form 
+                onSubmit={this.props.handleSubmit(this.props.createPost)} >
+                <h3>New Post</h3>
+                <label>Title</label><br/>
+                <input type="text" {...this.props.fields.title} /><br/>
+                <label>Categories</label><br/>
+                <input type="text" {...this.props.fields.categories} /><br/>
+                <label>Content</label><br/>
+                <textarea {...this.props.fields.content} /><br/>
+                <button type="submit">Add Post</button>
+            </form>
         )
     }
 }
 
-export default PostsNew
+export default reduxForm({
+    form: 'PostsNew',
+    fields: ['title', 'categories', 'content']
+    
+}, null, {createPost: createPost })(PostsNew)
