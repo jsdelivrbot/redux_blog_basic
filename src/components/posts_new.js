215cd97824d3335850108e600163bae80babdb36
Andy Eccleston <eandy5000@yahoo.com>
2017-01-07 09:30:04 -0600

    basic validation working

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,13 +5,18 @@ import { createPost } from '../actions/index'
 
 
 class PostsNew extends Component {
+   
+    
     render() {
+        const { fields: { title, categories, content } } = this.props
+
         return (
             <form 
                 onSubmit={this.props.handleSubmit(this.props.createPost)} >
                 <h3>New Post</h3>
                 <label>Title</label><br/>
-                <input type="text" {...this.props.fields.title} /><br/>
+                <input type="text" {...this.props.fields.title} />
+                <div>{ title.error }</div><br/>
                 <label>Categories</label><br/>
                 <input type="text" {...this.props.fields.categories} /><br/>
                 <label>Content</label><br/>
@@ -22,8 +27,19 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values){
+    const errors = {}
+
+    if(!values.title){
+        errors.title = "Enter a title"
+    }
+
+    return errors
+}
+
 export default reduxForm({
     form: 'PostsNew',
-    fields: ['title', 'categories', 'content']
+    fields: ['title', 'categories', 'content'],
+    validate: validate
     
 }, null, {createPost: createPost })(PostsNew)
