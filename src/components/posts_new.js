ba308a6c9237cc00fa0a067628ac9e35ad015389
Andy Eccleston <eandy5000@yahoo.com>
2017-01-07 12:14:12 -0600

    redirects to home on posts new submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 import { reduxForm } from 'redux-form'
 import { createPost } from '../actions/index'
 
 
 class PostsNew extends Component {
-   
+   static contextTypes = {
+       router: PropTypes.object
+   }
+
+   onSubmit(props) {
+       this.props.createPost(props)
+        .then(() => {
+            //once promise is fullfilled navigates user to '/'
+            this.context.router.push('/')
+        })
+   }
     
     render() {
         const { fields: { title, categories, content } } = this.props
 
         return (
             <form 
-                onSubmit={this.props.handleSubmit(this.props.createPost)} >
+                onSubmit={ this.props.handleSubmit(this.onSubmit.bind(this)) } >
                 <h3>New Post</h3>
                 <label>Title</label><br/>
                 <input type="text" {...this.props.fields.title} />
-                <div>{ title.error }</div><br/>
+                <div style={{color: "red"}}>{ title.touched ? title.error : '' }</div><br/>
                 <label>Categories</label><br/>
                 <input type="text" {...this.props.fields.categories} /><br/>
+                <div style={{color: "red"}}>{ categories.touched ? categories.error: ''}</div><br/>
                 <label>Content</label><br/>
                 <textarea {...this.props.fields.content} /><br/>
-                <button type="submit">Add Post</button>
+                <div style={{color: "red"}}>{ content.touched ? content.error: ''}</div><br/>
+                <button type="submit">Add Post</button><br/>
+                <Link to="/">Cancel</Link>
             </form>
         )
     }
@@ -33,6 +46,12 @@ function validate(values){
     if(!values.title){
         errors.title = "Enter a title"
     }
+    if(!values.categories){
+        errors.categories = "Enter a category"
+    }
+    if(!values.content) {
+        errors.content = "Blog post cannot be empty"
+    } 
 
     return errors
 }
